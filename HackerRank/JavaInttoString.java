db45baf70aad1f72bb9da8fbb3ac911c40d346dd
Torsa Times <50965423+TorsaTimes@users.noreply.github.com>
2023-04-06 22:58:32 +0200

    Create JavaInttoString.java

---
+++ HackerRank/JavaInttoString.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        try{
+           String s = Integer.toString(n);
+           
+           if(s instanceof String){
+               System.out.println("Good job");
+           }
+           else {
+               System.out.println("Good job");
+           }
+        } catch (Exception e){
+            System.out.println("Wrong answer");
+        }
+        sc.close();
+    }
+}
