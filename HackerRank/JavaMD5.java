be2b4abbf19f48f198b52fa377ff59f667779cb7
Torsa Times <50965423+TorsaTimes@users.noreply.github.com>
2023-04-06 22:31:05 +0200

    Create JavaMD5.java

---
+++ HackerRank/JavaMD5.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import  java.security.*;
+
+   // Java program to calculate MD5 hash value
+class MD5 {
+    
+    public static String getMd5(String input){
+        try {
+ 
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+ 
+            // digest() method is called to calculate message digest
+            // of an input digest() return array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+ 
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+ 
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+ 
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+ 
+public class Solution {
+    
+ 
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        
+       System.out.println(MD5.getMd5(s));
+    }
+}
