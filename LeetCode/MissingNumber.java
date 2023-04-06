58fd9af4a5346456218055f8df4fb950f8e5a4b5
Torsa Times <50965423+TorsaTimes@users.noreply.github.com>
2023-04-06 21:28:13 +0200

    Update MissingNumber.java

--- LeetCode/MissingNumber.java
+++ LeetCode/MissingNumber.java
@@ -1,9 +1,7 @@
 class Solution {
     public int missingNumber(int[] nums) {
         // sort array 
-        System.out.println(Arrays.toString(nums));
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
         int r = 0;
         for(int i = 0;i<nums.length;i++){
             System.out.println(nums[i] + " " + i);
