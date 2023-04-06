bfdfa590d184a08001fb91dc146806a95d2fbcfb
Torsa Times <50965423+TorsaTimes@users.noreply.github.com>
2023-04-06 22:44:23 +0200

    Create JavaFactoryPattern.java

---
+++ HackerRank/JavaFactoryPattern.java
@@ -0,0 +1,15 @@
+
+
+// Write your code here
+if(order.equals("cake")){
+    Cake c = new Cake();
+    return c;
+}
+if(order.equals("pizza")){
+    Pizza p = new Pizza();
+    return p;
+    
+}
+
+return null;
+
